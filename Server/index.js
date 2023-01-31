bbaf167a3cd067bc90d723e85ab60bc03319d3ef
bugger69 <aatreya21@gmail.com>
2023-01-31 18:19:24 +0530

    passport under construction

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,20 @@
-if(process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
+const session = require("express-session");
 const cors = require("cors");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const MongoDBStore = require("connect-mongo");
 
 const uploadRoutes = require("./routes/uploadRoutes");
 const userRoutes = require("./routes/userRoutes");
+
+const User = require("./models/users");
 
 //setting up express
 const app = express();
@@ -21,12 +27,51 @@ app.use(bodyparser.json());
 
 main().catch((err) => console.log(err));
 
+const db_url = process.env.DB_URL || "mongodb://localhost:27017/ResumeManager";
+
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/ResumeManager");
+  await mongoose.connect(db_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   console.log("Mongo connection open");
 }
 
-app.use('/', userRoutes);
+const secret = process.env.SECRET_KEY || "fuckOffIdiots";
+
+const store = new MongoDBStore({
+  mongoUrl: db_url,
+  secret: secret,
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("Session Store Error.", e);
+});
+
+const sessionConfig = {
+  store,
+  name: process.env.SESSION_NAME || "notasession",
+  secret: secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use("/", userRoutes);
 app.use("/upload", uploadRoutes);
 
 app.listen(port, () => {
