59371f00f5d15f669ec9530857faf34e0270902a
bugger69 <aatreya21@gmail.com>
2023-02-02 14:47:50 +0530

    updated login

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,29 +1,48 @@
-import axios from 'axios';
-import React, {useState} from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+
+import "./login.css";
 
 const Login = () => {
-    const [rollNo, setRollNo] = useState();
-    const [dob, setDob] = useState();
-    // post to http://localhost:4000/
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        const obj = {name: rollNo, password: dob };
-        axios.post("http://localhost:4000/login", obj)
-        .then((res) => {
-            alert("Logged in!!!");
-            window.location.href = "/";
-        }).catch(e => {
-            console.log(e);
-        })
-    }
-    return <form onSubmit={onSubmit}>
-    <p>Roll No.</p>
-    <input type = "text" id = "name" onChange={(e) => setRollNo(e.target.value)} />
-    <p>Date of birth</p>
-    <input type = "password" id = "password" onChange={(e) => setDob(e.target.value)} />
-    <button type ="submit" value = "submit">Submit</button>
-</form>
-}
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const obj = { name: username, password: password };
+    axios
+      .post("http://localhost:4000/login", obj)
+      .then((res) => {
+        alert("Logged in!!!");
+        window.location.href = "/";
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  return (
+    <div className="cont">
+      <form className="input" onSubmit={onSubmit}>
+        <label className="label" htmlFor="username">Username:</label>
+        <input
+          className="input"
+          type="text"
+          id="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label className="label" htmlFor="password">Password:</label>
+        <input
+          className="input"
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="button" type="submit" value="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
