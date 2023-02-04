00a82e6033016c27398523bf81117a9bc9d70774
bugger69 <aatreya21@gmail.com>
2023-02-04 15:49:09 +0530

    .

--- Server/models/resume.js
+++ Server/models/resume.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 const resumeSchema = new mongoose.Schema({
     fileId: String,
     fileName: String
-});
+}, {timestamps: true});
 
 module.exports = mongoose.model("resume", resumeSchema);
