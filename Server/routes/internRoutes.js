0b38a4d8f3e54a737e614d05f48bb07b8a630b24
bugger69 <aatreya21@gmail.com>
2023-04-07 20:23:07 +0530

    even more fixes

--- Server/routes/internRoutes.js
+++ Server/routes/internRoutes.js
@@ -122,15 +122,14 @@ router.post("/:internId", isLoggedIn, async (req, res, next) => {
     const resume = await Resume.findById(req.user.resumes[size - 1]);
     console.log(resume);
 
-    const application = {Resume: resume, User: user};
+    const application = { Resume: resume, User: user };
 
     intern.applications.push(application);
     const response = await intern.save();
 
     console.log(response);
 
-   
-    res.status(200).send({msg: "application successfull"});
+    res.status(200).send({ msg: "application successfull" });
   } catch (e) {
     console.log(e);
     res.status(400).send({ msg: "An error occured" });
@@ -139,7 +138,8 @@ router.post("/:internId", isLoggedIn, async (req, res, next) => {
 
 // get applications (recruiter only)
 
-router.get("/applications/:internId", isLoggedIn, async (req, res, next) => { // needs a ton of fixing
+router.get("/applications/:internId", isLoggedIn, async (req, res, next) => {
+  // needs a ton of fixing
   try {
     const internId = req.params.internId;
     const intern = await Intern.findById(internId);
@@ -147,14 +147,10 @@ router.get("/applications/:internId", isLoggedIn, async (req, res, next) => { //
     if (!intern) {
       throw new Error("No intern found");
     }
-    // await intern.populate("applications.Resume");
-    // await intern.populate("applicatons.User");
+
     const zip = new JSZip();
     for (let application of intern.applications) {
       if (application) {
-        const stud = application.Resume;
-        const stud1 = application.User;
-       
         const resumeId = application.Resume.toString();
         const userId = application.User.toString();
 
@@ -166,16 +162,10 @@ router.get("/applications/:internId", isLoggedIn, async (req, res, next) => { //
         const fileName = resume.fileName;
         const bucket = await GetBucket();
         console.log(bucket);
-        // console.log(bucket);
+
         const bucketId = bucket.buckets[0].bucketId;
-        // const bucketName = bucket.buckets[0].bucketName;
 
         let fileId = resume.fileId;
-        // const fileInfo = await b2.getFileInfo({ fileId });
-
-        // const { data: { buckets } } = await b2.listBuckets();
-        // const buck = buckets.find(bucket => bucket.bucketId === bucketId);
-        // const bucketName = buck.bucketName;
 
         const auth = await b2.getDownloadAuthorization({
           bucketId: bucketId,
@@ -189,13 +179,13 @@ router.get("/applications/:internId", isLoggedIn, async (req, res, next) => { //
           responseType: "json",
           onDownloadProgress: (event) => {},
         });
- 
-        zip.file(fileName, file.data );
+
+        zip.file(fileName, file.data);
         zip.file(`${user.username}.pdf`, JSON.stringify(user));
       }
     }
 
-    const zipData = await zip.generateAsync({ type: 'nodebuffer' });
+    const zipData = await zip.generateAsync({ type: "nodebuffer" });
     res.status(200).send(zipData);
   } catch (e) {
     console.log(e);
